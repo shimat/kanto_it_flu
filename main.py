1cda88b071379cad16e5edf2b3d742e726954a9e
shimat <schimatk@gmail.com>
2024-11-05 13:44:28 +0900

    fix

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
 import sys
 
+import folium
 import pandas as pd
 import streamlit as st
-import folium
 from streamlit_folium import st_folium
 
 from css import write_css
@@ -28,7 +28,7 @@ df_main.rename(
 )
 df_coords = pd.read_csv("address_coordinates.csv", header=0, usecols=("address", "longitude", "latitude"))
 
-df = df_main.merge(df_coords, left_on="住所", right_on="address", how="left")
+df = df_main.merge(df_coords, left_on="住所", right_on="address", how="left").drop("address", axis=1)
 # st.write(df)
 last_update = df_head.iloc[0, 7]
 
@@ -76,9 +76,7 @@ def tab2(tab, df) -> None:
     with tab:
         col1, col2 = st.columns([3, 2])
         with col1:
-            query = col1.text_input(
-                label="住所検索", value="千代田区", help="入力した文字列と一致、または正規表現にマッチする住所でリストアップします"
-            )
+            query = col1.text_input(label="住所検索", help="入力した文字列と一致、または正規表現にマッチする住所でリストアップします")
         with col2:
             search_option = col2.selectbox("検索方法", ("部分一致", "先頭一致", "正規表現"))
 
@@ -89,25 +87,30 @@ def tab2(tab, df) -> None:
                 is_regex = search_option == "正規表現"
                 df = df[df["住所"].str.contains(query, regex=is_regex)]
 
-        df["医療機関名称"] = df["医療機関名称"].apply(lambda s: f"https://www.google.com/search?q={s}")
-        df["住所"] = df["住所"].apply(lambda s: f"<a target='_blank' href='https://www.google.com/maps/search/?api=1&query={s}'>{s}</a>")
+        # TODO: See the caveats in the documentation: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
+        df.loc[:, "医療機関名称Raw"] = df.loc[:, "医療機関名称"]
+        df.loc[:, "医療機関名称"] = df["医療機関名称"].apply(
+            lambda s: f"<a target='_blank' href='https://www.google.com/search?q={s}'>{s}</a>"
+        )
+        df.loc[:, "住所"] = df["住所"].apply(
+            lambda s: f"<a target='_blank' href='https://www.google.com/maps/search/?api=1&query={s}'>{s}</a>"
+        )
 
-        html = df.to_html(escape=False)
+        html = df.to_html(escape=False, columns=("医療機関名称", "住所", "電話番号", "料金(税込)", "医療機関通信欄"))
         st.write(html, unsafe_allow_html=True)
-        st.dataframe(df, hide_index=True, column_config={"医療機関名称": st.column_config.LinkColumn(display_text="q=(.*?)")})
 
         df_map = df.head(1000)
         folium_map = folium.Map(
             location=[df_map["latitude"].mean(), df_map["longitude"].mean()],
             # tiles="https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png",
             # attr='&copy; <a href="https://maps.gsi.go.jp/development/ichiran.html">国土地理院</a>',
-            zoom_start=10,
+            zoom_start=8,
         )
         for _, row in df_map.iterrows():
             folium.Marker(
                 location=[row["latitude"], row["longitude"]],
-                popup=folium.Popup(f"<p>{111}</p>", max_width=300),
-                tooltip="AAA",
+                popup=folium.Popup(f"<b>{row["医療機関名称Raw"]}</b>", max_width=300),
+                tooltip=f"{row["医療機関名称Raw"]}",
                 icon=folium.Icon(color="red", prefix="fa", icon="hospital-alt"),
             ).add_to(folium_map)
         st.header("地図表示 (最大1000件)")
